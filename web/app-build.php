d447aaeb1e6db92584b4010845a7e1475b96359d
Carles Climent <carlescliment@hotmail.com>
2014-05-16 16:00:45 +0000

    Add redirections

--- web/app-build.php
+++ web/app-build.php
@@ -3,7 +3,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php'; 
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\RedirectResponse;
 
 
 $app = new Silex\Application();
@@ -17,7 +18,7 @@ $app->get('/', function(Silex\Application $app, Request $request) {
             $value = $game->execute($request->get('command'));        
         }
         
-        return '<form><input class="command" name="command" /></form><p>' . $value . '</p>';
+        return '<form><input class="command" name="command" /><input type="submit" value="Send" /></form><p>' . $value . '</p>';
     } 
     return '<a href="/app.php/start">Start game</a>';
 });
@@ -26,13 +27,13 @@ $app->get('/start', function(Silex\Application $app, Request $request) {
     $game_creator = $app['game_provider'];
     $game = $game_creator->getGame();
     $app['session']->set('game', $game);
-    return '';
+    return new RedirectResponse('/');
 });
 
 
 $app->get('/end', function(Silex\Application $app, Request $request) { 
     $app['session']->remove('game');
-    return '';
+    return new RedirectResponse('/');
 });
 
 return $app;
